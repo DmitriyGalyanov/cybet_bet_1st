6665d72ea8b05c22c83e2146c8bd6ef6b3bc0c72
DmitriyGalyanov <dimagalyanov@gmail.com>
2021-02-12 02:49:10 +0300

    alter helpers.createMatches

--- src/helpers.js
+++ src/helpers.js
@@ -12,7 +12,6 @@ export const getRandomIntInclusive = (min, max) => {
 import {Animated, Easing} from 'react-native';
 
 /**
- * 
  * @param {React.MutableRefObject<Animated.Value>} animatableValue
  * @param {number} toValue
  * @param {number} animDuration in milliseconds
@@ -26,7 +25,6 @@ export const animateValue = (animatableValue, toValue, animDuration) => {
 };
 
 /**
- * 
  * @param {React.MutableRefObject<Animated.Value>} animatableValue
  * @param {number} toValue
  * @param {number} animDuration in milliseconds
@@ -40,10 +38,19 @@ export const easingAnimateValue = (animatableValue, toValue, animDuration) => {
 	}).start();
 };
 
+/**
+ * @param {number} month
+ * @param {number} year
+ */
+const daysInMonth = (month, year) => {
+	return new Date(year, month, 0).getDate();
+};
+
 import { initialMatchesAmount } from "./constants";
 import {teamsAmount} from '../assets/images';
 
 /**
+ * function is clunky since it was made for single discipline matches creation
  * @returns array filled with matches data (objects) needed to create a PlayField
  */
 export const createMatches = () => {
@@ -62,6 +69,37 @@ export const createMatches = () => {
 		let matchSeconds = getRandomIntInclusive(0, 59);
 		if (matchSeconds < 10) matchSeconds = `0${matchSeconds}`;
 		const matchTimeString = `${matchMinutes}:${matchSeconds}`;
+
+		const currentDate = new Date();
+		const currentYear = currentDate.getFullYear();
+		const currentMonth = currentDate.getMonth();
+		const currentDay = currentDate.getDate();
+		const currentHour = currentDate.getHours();
+		const currentMinute = currentDate.getMinutes();
+
+		const daysInCurrentMonth = daysInMonth(currentMonth + 1, currentYear);
+
+		let matchMonth = (currentMonth + 1 < 10) ? `0${currentMonth + 1}` : `${currentMonth + 1}`;
+		let matchDay = getRandomIntInclusive(currentDay, daysInCurrentMonth);
+		if (currentHour > 22) matchDay = getRandomIntInclusive(currentDay + 1, daysInCurrentMonth);
+		if (currentDay === daysInCurrentMonth) {
+			matchDay = getRandomIntInclusive(1, 10);
+			matchMonth = ((currentDate.getMonth() + 1) + 1 < 10)
+				? `0${currentDate.getMonth() + 2}`
+				: `${currentDate.getMonth() + 2}`;
+		};
+
+		let matchHour = matchDay === currentDay ?
+			getRandomIntInclusive(currentHour, 23) : getRandomIntInclusive(0, 23);
+		if (matchHour < 10) matchHour = `0${matchHour}`;
+		let matchMinute = matchDay === currentDay ?
+			getRandomIntInclusive(currentMinute, 59) : getRandomIntInclusive(0, 59);
+		matchMinute = Math.round(matchMinute / 5) * 5;
+		if (matchMinute < 10) matchMinute = `0${matchMinute}`
+		else if (matchMinute === 60) matchMinute = 55;
+
+		const matchStartDate = `${matchDay}.${matchMonth}.${currentYear.toString().substring(2)}`;
+		const matchStartTime = `${matchHour}:${matchMinute}`;
 
 		const coefficients = [
 			{
@@ -84,12 +122,15 @@ export const createMatches = () => {
 		const match = {
 			matchId: i,
 			firstTeamId: firstTeamId,
-			matchCurrentTime: matchTimeString,
+			matchCurrentTime: matchTimeString, ///
+			matchStartDate: matchStartDate,
+			matchStartTime: matchStartTime,
 			secondTeamId: secondTeamId,
 			coefficients: coefficients,
+			belongsTo: 'footballPredictionsScreen',
 		};
 		matchesArray.push(match);
 	};
 
-	return matchesArray
+	return matchesArray;
 };
