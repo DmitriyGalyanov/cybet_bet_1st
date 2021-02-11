7c912f7c9345e36ac767eeecccb14932ab109e23
DmitriyGalyanov <dimagalyanov@gmail.com>
2021-02-12 02:46:03 +0300

    add src/modalParts

---
+++ src/modalParts/index.js
@@ -0,0 +1,9 @@
+import DecisionModal from './DecisionModal';
+import BetSuccessModal from './BetSuccessModal';
+import BetFailModal from './BetFailModal';
+
+export {
+	DecisionModal,
+	BetSuccessModal,
+	BetFailModal,
+};
