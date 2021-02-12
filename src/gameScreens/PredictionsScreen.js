5aa8b167f185473dab2c0290108133938a1d3109
DmitriyGalyanov <dimagalyanov@gmail.com>
2021-02-12 14:30:31 +0300

    add sort by time to PredictionsScreen

--- src/gameScreens/PredictionsScreen.js
+++ src/gameScreens/PredictionsScreen.js
@@ -24,6 +24,18 @@ export default function PredictionsScreen ({route}) {
 	const matchesData = useSelector(selectGameData).matchesDataArray
 		.filter(match => match.belongsTo === routeName);
 
+	const displayedMatchesData = matchesData.sort((matchA, matchB) => {
+		const [dayA, monthA, yearA] = matchA.matchStartDate.split('.');
+		const fullYearA = +`20${yearA}`;
+		const matchDateA = new Date(fullYearA, monthA - 1, dayA);
+
+		const [dayB, monthB, yearB] = matchB.matchStartDate.split('.');
+		const fullYearB = +`20${yearB}`;
+		const matchDateB = new Date(fullYearB, monthB - 1, dayB);
+
+		return (matchDateA.getTime() - matchDateB.getTime());
+	});
+
 	return (
 		<ScrollView
 			contentContainerStyle={{
@@ -32,7 +44,7 @@ export default function PredictionsScreen ({route}) {
 				maxWidth: windowWidth,
 			}}
 		>
-			{matchesData.map(match => {
+			{displayedMatchesData.map(match => {
 				const {
 					matchId,
 					coefficients,
