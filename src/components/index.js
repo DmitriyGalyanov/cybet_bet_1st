4937c360120dadf329760aa65214dc37057f5e2b
DmitriyGalyanov <dimagalyanov@gmail.com>
2021-02-13 17:21:42 +0300

    remove deprecated Components imports

--- src/components/index.js
+++ src/components/index.js
@@ -8,9 +8,6 @@ import BottomModal from './BottomModal';
 
 import TeamImg from './TeamImg';
 import MatchTile from './MatchTile';
-import RadioButtonsGroup from './RadioButtonsGroup';
-import BackgroundedHeader from './BackgroundedHeader';
-import MainButton from './MainButton';
 
 
 export {
@@ -24,7 +21,4 @@ export {
 
 	TeamImg,
 	MatchTile,
-	RadioButtonsGroup,
-	BackgroundedHeader,
-	MainButton,
 };
--- src/gameScreens/index.js
+++ src/gameScreens/index.js
@@ -1,15 +1,7 @@
 import MainGameScreen from './MainGameScreen';
 import PredictionsScreen from './PredictionsScreen';
 
-import MatchesListScreen from './MatchesListScreen';
-import MakeBetScreen from './MakeBetScreen';
-import BetResultScreen from './BetResultScreen';
-
 export {
 	MainGameScreen,
 	PredictionsScreen,
-
-	MatchesListScreen,
-	MakeBetScreen,
-	BetResultScreen,
 };
