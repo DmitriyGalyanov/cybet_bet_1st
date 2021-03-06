4cba2a2ff1cd53c54fe3ef17345e3863b4798561
Dmitriy Galyanov <dimagalyanov@gmail.com>
2021-03-06 22:00:51 +0300

    add redux/store (persisted)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,48 @@
-import { configureStore } from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import {gameSliceReducer} from './stateSlices';
-import {modalSliceReducer} from './stateSlices';
+import {
+	combineReducers,
+	configureStore,
+	getDefaultMiddleware,
+} from '@reduxjs/toolkit';
 
-export default configureStore({
-	reducer: {
-		gameSlice: gameSliceReducer,
-		modalSlice: modalSliceReducer,
-	},
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from 'redux-persist';
+
+import {
+	gameSliceReducer,
+	modalSliceReducer,
+	webViewSliceReducer,
+} from './stateSlices';
+
+const persistConfig = {
+	key: 'root',
+	storage: AsyncStorage,
+};
+
+const rootReducer = combineReducers({
+	gameSlice: gameSliceReducer,
+	modalSlice: modalSliceReducer,
+	webViewSlice: webViewSliceReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+	reducer: persistedReducer,
+	middleware: getDefaultMiddleware({
+		serializableCheck: {
+			ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+		}
+	})
+});
+
+export const persistor = persistStore(store);
